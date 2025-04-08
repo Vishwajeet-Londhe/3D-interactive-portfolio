2c86a0dc6f83a0c1705e702fbb7ec9acea952e2c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-04-08 09:55:48 +0530

    hope

--- src/jobs.js
+++ src/jobs.js
@@ -1,18 +1,38 @@
 // Jobs.js
 import React from "react";
-import { Link } from "react-router-dom";
-import "./Jobs.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Jobs() {
+  const navigate = useNavigate();
+
+  const handleGephelsClick = () => {
+    navigate("/jobs/Gephels");
+  };
+
   return (
     <div className="jobs-page section">
       <h2>Job and Internship Opportunities</h2>
+
       <ul>
         <li>
-          <Link to="/jobs/Gephels">Gephels Pvt. Ltd.</Link>
+          <Link to="/jobs/Gephels">Gephels Pvt. Ltd. (Link)</Link>
         </li>
-        {/* You can add more jobs here */}
       </ul>
+
+      <button
+        onClick={handleGephelsClick}
+        style={{
+          marginTop: "20px",
+          padding: "10px 20px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer"
+        }}
+      >
+        View Gephels Job
+      </button>
     </div>
   );
 }
